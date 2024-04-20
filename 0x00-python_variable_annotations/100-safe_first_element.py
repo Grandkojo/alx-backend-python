0f738f04f1b5b9dd899d1b3e53188ceb6686469e
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-04-20 00:18:24 +0000

    python annotations

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bn/env python3
+"""Duck typing annotation"""
+from typing import List, Union, Any, Sequence
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Returns the first element of s list"""
+    if lst:
+        return lst[0]
+    else:
+        return None
