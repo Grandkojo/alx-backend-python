832c7be866801e189e084b3429e75bfabc2c714c
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-05-13 16:42:33 +0000

    unittesting

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import unittest
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test access_nested_map
+    """
+
+    @parameterized.expand
+    def test_access_nested_map():
+        """Test access nested map function
+        """
